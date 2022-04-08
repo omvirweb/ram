120ad5a09c448754eecdc86d41098e026b4fa3df
paragempyreal <parag.empyreal1@gmail.com>
2022-04-08 10:19:30 +0530

    changes in line item for sales invoice type 3

--- application/views/line_items.php
+++ application/views/line_items.php
@@ -67,7 +67,7 @@
     	}
     } ?>
 
-    <?php if($voucher_type == 'sales2') { ?>
+    <?php if($voucher_type == 'sales2' || $voucher_type == 'sales3') { ?>
         <div class="col-md-3">
             <div class="form-group">
                 <label for="line_item_des" class="control-label">Item Description</label>
@@ -408,7 +408,7 @@
 		//initAjaxSelect2($("#item_id"),"<?=base_url('app/li_item_select2_source/')?>");
 		initAjaxSelect2($("#item_id"),"<?=base_url('app/item_select2_source/')?>");
 
-        <?php if($voucher_type != 'sales2') { ?>
+        <?php if($voucher_type != 'sales2' || $voucher_type == 'sales3') { ?>
 		  initAjaxSelect2($("#unit_id"),"<?=base_url('app/unit_select2_source_by_item_id/')?>");
         <?php } else {?>
             initAjaxSelect2($("#unit_id"),"<?=base_url('app/unit_select2_source/')?>");
@@ -678,7 +678,7 @@
 		
 		$('#add_lineitem').on('click', function() {
 			var item_id = $("#item_id").val();
-            <?php if($voucher_type != "sales2") { ?>
+            <?php if($voucher_type != "sales2" && $voucher_type != 'sales3') { ?>
 			if(item_id == '' || item_id == null){
 				show_notify("Please select Product.", false);
 				return false;
@@ -1230,7 +1230,7 @@
         	setSelect2Value($("#sub_cat_id"),"<?=base_url('app/set_sub_category_select2_val_by_id/')?>" + value.sub_cat_id);
         }
 
-        <?php if($voucher_type != 'sales2') { ?>
+        <?php if($voucher_type != 'sales2' || $voucher_type == 'sales3') { ?>
             initAjaxSelect2($("#unit_id"),"<?=base_url('app/unit_select2_source_by_item_id/')?>" + value.item_id);
             if(typeof(value.unit_id) !== "undefined" && value.unit_id !== null && value.unit_id !== 0) {
             	setSelect2Value($("#unit_id"),"<?=base_url('app/set_pack_unit_select2_val_by_id/')?>" + value.unit_id);
