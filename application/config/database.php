aad4d4edc758600021c30721c56a3790b3181992
Kaushik <kaushiksondarva.technocomet@gmail.com>
2024-02-05 15:21:47 +0530

    Database name revert

--- application/config/database.php
+++ application/config/database.php
@@ -77,8 +77,8 @@ $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
 	'username' => 'root',
-	'password' => '',  // DB@RamConstructions@2023
-	'database' => 'ramlive',
+	'password' => 'DB@RamConstructions@2023',
+	'database' => 'rambirth',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
@@ -95,4 +95,4 @@ $db['default'] = array(
 	'save_queries' => TRUE
 );
 
-// $db['default']['port'] = 3308;
+$db['default']['port'] = 3308;
